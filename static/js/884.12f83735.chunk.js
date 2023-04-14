764acb54e7f6d810e2c2d1bf13c0dd2be12884ee
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-04-14 14:45:55 +0000

    Deploying to gh-pages from @ AndriyNich/goit-react-hw-05-movies@b852c99fdb3c3ef79ffec7ab9b7d0ff51c229d27 🚀

---
+++ static/js/884.12f83735.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[884],{884:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"MovieDetails"})}}}]);
+//# sourceMappingURL=884.12f83735.chunk.js.map
